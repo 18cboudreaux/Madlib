53141beb48023c6a0a050ba32fb187ac1b33508b
Coder <coder@mainlab13.local>
2016-01-06 11:18:11 -0800

    Added menu

--- madlib/main.swift
+++ madlib/main.swift
@@ -19,3 +19,6 @@ func input () -> String {
     return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
 }
 
+print("Menu \nPlease choose an option \n(1)\n(2)\n(3)\n(4)\n(5)\n(6) No")
+var menu = input()
+
