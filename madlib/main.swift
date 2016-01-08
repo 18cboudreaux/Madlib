6b720a9a7ee053e8e4df91a07ab679a08529fffe
Coder <coder@mainlab13.local>
2016-01-08 11:10:45 -0800

    ghjfjhgck

--- madlib/main.swift
+++ madlib/main.swift
@@ -10,6 +10,18 @@
 //Group Name: Madlib
 //Members: Gabe E., Risen J., Chase B.
 
+//
+//  main.swift
+//  madlib
+//
+//  Created by Coder on 1/6/16.
+//  Copyright © 2016 Coder. All rights reserved.
+//
+
+
+//Group Name: Madlib
+//Members: Gabe E., Risen J., Chase B.
+
 import Foundation
 
 func input () -> String {
@@ -20,39 +32,37 @@ func input () -> String {
     return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
 }
 
-print("Menu \nPlease choose an option \n(1) Lunch room\n(2) Animals\n(3) Sports\n(4) Recess\n(5) Pizza\n(6) No")
-var menu = input()
-
-func lunchRoom() {
+func lunchRoom(){
     
     //It will error till we have it all printed. Do not worry till then.
     
-print("container")
-let blankOne = input()
-
-print("adjective")
-let blankTwo = input()
-
-print("adjective")
-let blankThree = input()
-
-print("noun")
-let blankFour = input()
-
-print("animal")
-let blankFive = input()
+    print("container")
+    let blankOne = input()
     
-print("vegetable")
-let blankSix = input()
-
-print("vegetable")
-let blankSeven = input()
+    print("adjective")
+    let blankTwo = input()
     
-print("color")
-let blankEight = input()
+    print("adjective")
+    let blankThree = input()
     
-print("adjective")
-let blankNine = input()
+    print("noun")
+    let blankFour = input()
+    
+    print("animal")
+    let blankFive = input()
     
     
+    //print("This is a test; \(blankOne) \(blankTwo) \(blankThree) \(blankFour) \(blankFive)")
+}
+
+
+print("Menu \nPlease choose an option \n(1) Lunch room\n(2) Animals\n(3) Sports\n(4) Recess\n(5) Pizza\n(6) No")
+var menu = input()
+
+for menu in 1...6 {
+    
+    if menu == 1 {
+        lunchRoom()
+        
+    }
 }
