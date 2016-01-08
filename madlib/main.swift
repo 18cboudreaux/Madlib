c5b3d3f701760c2b210e33a81b5a0fafc7ff3257
Coder <coder@mainlab13.local>
2016-01-08 11:25:30 -0800

    dfasdgfsfdhgsdf

--- madlib/main.swift
+++ madlib/main.swift
@@ -48,9 +48,40 @@ func lunchRoom(){
     print("noun")
     let blankFour = input()
     
-print("adjective")
-let blankNine = input()
+    print("adjective")
+    let blankNine = input()
    
-print("Make sure your lunch \(blankOne) is filled with nutritious \(blankTwo) food. Do not go to the \(blankThree) food stand across the stret from the school. The hamburgers they serve from are fried in \(blankFour) and are made of \(blankFive) meat. So take a sandwich made of \(blankSix) or \(blankSeven) it's much healthier! Drink \(blankEight) milk instead of \(blankNine) colas.")
+    //print("Make sure your lunch \(blankOne) is filled with nutritious \(blankTwo) food. Do not go to the \(blankThree) food stand across the stret from the school. The hamburgers they serve from are fried in \(blankFour) and are made of \(blankFive) meat. So take a sandwich made of \(blankSix) or \(blankSeven) it's much healthier! Drink \(blankEight) milk instead of \(blankNine) colas.")
 }
 
+func animals(){
+    
+}
+
+func sports(){
+    
+}
+
+func recess(){
+    
+}
+
+
+
+
+
+
+print("Menu \nPlease choose an option \n(1) Lunch room\n(2) Animals\n(3) Sports\n(4) Recess\n(5) Pizza\n(6) No")
+var menu = input()
+
+for menu in 1...6 {
+    
+    if menu == 1 {
+        lunchRoom()
+        
+    }
+    
+    else if menu == 2 {
+        animals()
+    }
+}
