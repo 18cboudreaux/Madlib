90da36403a36a14ecb1a0ec93e7e0043babfed64
Coder <coder@mainlab13.local>
2016-01-14 11:11:56 -0800

    fixed menu

--- madlib/main.swift
+++ madlib/main.swift
@@ -64,6 +64,18 @@ func lunchRoom(){
     let blankNine = input()
    //The work of Risen
     print("Make sure your lunch \(blankOne) is filled with nutritious \(blankTwo) food. Do not go to the \(blankThree) food stand across the stret from the school. The hamburgers they serve from are fried in \(blankFour) and are made of \(blankFive) meat. So take a sandwich made of \(blankSix) or \(blankSeven) it's much healthier! Drink \(blankEight) milk instead of \(blankNine) colas.")
+    
+    print("")
+    
+    print("Would you like to play again? \nYes \nOr \nNo")
+    var reply = input()
+    
+    if reply == "Yes" {
+        menu()
+    }
+    else if reply == "No"{
+        print("Thank you, goodbye.")
+    }
 }
 // The work of Gabe
 func animals(){
@@ -104,6 +116,18 @@ func animals(){
     let spot12 = input()
     //The work of Risen
     print("Today I went to the zoo. I saw a \(spot1) \(spot2) jumping up and down in its trees. He \(spot3) \(spot4) through the large tunnel that led to its \(spot5) \(spot6). I got some peanuts and passed them through the cage to a gigantic gray \(spot7) towering above my head. Feeding that animal made me hungry. I went to go get a \(spot8) scoop of ice cream. It filled my stomach. Afterwards I had to \(spot9) \(spot10) to catch our bus. When I got home I \(spot11) my mom for a \(spot12) day at the zoo.")
+    
+        print("")
+        
+        print("Would you like to play again? \nYes \nOr \nNo")
+    var reply = input()
+    
+    if reply == "Yes" {
+        menu()
+    }
+    else if reply == "No"{
+        print("Thank you, goodbye.")
+    }
 
 }
 // The work of Gabe
@@ -142,6 +166,18 @@ func sports(){
     let open11 = input()
     // The work of Risen
     print("BREAKING NEWS: \(open1), who plays \(open2) for the \(open3), was arrested lastnight for \(open4). Officals say he had been \(open5) and was armed with a \(open6) and there was \(open7) everywhere at the scence of the crime. NFL Commissioner Rgoer Goodell plans to suspend him for \(open8) games and fine him $\(open9). This marks the NFL's \(open10) arrest since \(open11).")
+    
+    print("")
+    
+    print("Would you like to play again? \nYes \nOr \nNo")
+    var reply = input()
+    
+    if reply == "Yes" {
+        menu()
+    }
+    else if reply == "No"{
+        print("Thank you, goodbye.")
+    }
 }
 //The work of Gabe
 func recess(){
@@ -173,10 +209,24 @@ func recess(){
     let fill9 = input()
     //The work of Risen
    print("Here are some things to do at recess. 1. Start a game pf touch \(fill1)-ball. 2. Put a \(fill2) un someone's lunch bag. 3. Start a \(fill3) fight in the school \(fill4) room. 4. Choos sides and have a \(fill5) ball tournament. 5. Demand more \(fill6) and shorter \(fill7) classes. 6. Choose \(fill8) kids to be it at \(fill9)")
+    
+    print("")
+    
+    print("Would you like to play again? \nYes \nOr \nNo")
+    var reply = input()
+    
+    if reply == "Yes" {
+        menu()
+    }
+    else if reply == "No"{
+        print("Thank you, goodbye.")
+    }
 }
+
+
 // The work of Gabe
 
-func Random(){
+func oneDay() {
     print("Proper noun")
     let slip1 = input()
     
@@ -200,28 +250,44 @@ func Random(){
     // The work of Risen
     print("One day \(slip1) was \(slip2) to the \(slip3). On the way, \(slip4) saw a \(slip5). This was a suprise so \(slip6) \(slip7) quickly.")
     
+print("")
     
+print("Would you like to play again? \nYes \nOr \nNo")
+    var reply = input()
     
-    
-    
+    if reply == "Yes" {
+        menu()
+}
+    else if reply == "No"{
+        print("Thank you, goodbye.")
+}
 }
 
+func menu() {
 
+print("Menu \nPlease choose an option \n(1) Lunch room\n(2) Animals\n(3) Sports\n(4) Recess\n(5) One Day\n(6) Quit")
+var menuOption = input()
 
-
-
-print("Menu \nPlease choose an option \n(1) Lunch room\n(2) Animals\n(3) Sports\n(4) Recess\n(5) Random\n(6) No")
-var menu = input()
-
-for menu in 1...6 {
-    
-    if menu == 1 {
+    if menuOption == "1" {
         lunchRoom()
-        
     }
-    
-    else if menu == 2 {
+    else if menuOption == "2" {
         animals()
         
     }
+    else if menuOption == "3" {
+        sports()
+    }
+    else if menuOption == "4" {
+        recess()
+    }
+    else if menuOption == "5" {
+        oneDay()
+    }
+    else if menuOption == "6" {
+        print("Thank you, goodbye.")
 }
+}
+
+
+menu()
